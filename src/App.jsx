01bae2e1cf07fad258c76604535f1b1d91e26768
Akshay <akshay1.prasad@ril.com>
2025-08-01 20:00:19 +0530

    Add App component and update index.html to reference it correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import Details from "./Details";
+import Details from "./components/Details";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import AdoptedPetContext from "../context/AdoptedPetContext";
+import SearchParams from "./components/SearchParams";
+import AdoptedPetContext from "./context/AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
